ad6b9d8529a1e38b74b12a239f48adb1e72a4344
mananispiwpiw <rafijaeger@gmail.com>
2024-01-25 11:58:19 +0700

    add logic to test API response

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Mom!")
+	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=c01fc842f0664034bda42040242501&q=Jakarta&aqi=no")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API not available!")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
