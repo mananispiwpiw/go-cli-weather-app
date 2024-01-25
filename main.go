738a1bb97f40168606247010073673e9e8538e8e
mananispiwpiw <rafijaeger@gmail.com>
2024-01-25 16:57:55 +0700

    create Weather struct to store the data from API

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
+// Struct to store data from API
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct { //Current status
+		Temp_c    float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		}
+		Uv float64 `json:"uv"`
+	} `json:"current"`
+	Forecast struct { //Forecasting or prediction
+		Forecasatday []struct {
+			Hour []struct {
+				Time_epoch     int     `json:"time_epoch"`
+				Temp_c         float64 `json:"temp_c"`
+				Chance_of_rain float64 `json:"chance_of_rain"`
+				Condition      struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	}
+}
+
 func main() {
-	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=c01fc842f0664034bda42040242501&q=Jakarta&aqi=no")
+
+	res, err := http.Get(os.Getenv("WEATHER_API_URL_1"))
 	if err != nil {
 		panic(err)
 	}
@@ -22,5 +52,11 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	//Store the data from WeatherAPI to struct
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(weather)
 }
